c0aabeca6fd5cb0ff4d8ea24285bfc32451e8e64
leunbin <silverbin2105@gmail.com>
2026-02-12 16:02:32 +0900

    fix.기본 컨테이너

--- styles/styles.tsx
+++ styles/styles.tsx
@@ -2,6 +2,15 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+
+    width: '100%',
+    height: '100%',
+
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
